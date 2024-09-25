019173c014e4482a83cb1abac985b3486450c2af
sunnybhardwaj2027 <sunnybhardwaj7549@gmail.com>
2024-09-25 22:42:20 +0530

    merge two sorted array without using any extra space

---
+++ 88_merge_sorted_array.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] >= nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    vector<int> nums2 = {2, 5, 6};
+    int m = 3;
+    int n = 3;
+
+    sol.merge(nums1, m, nums2, n);
+
+    for (int num : nums1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
